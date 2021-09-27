8c3700de9a4d8760809d6a3c8934ca7548ffce92
Wiwida <dassiewilhelm@gmail.com>
2021-09-27 21:44:12 +0200

    Mise en place sur git

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="container__app">
+    <router-view/>
   </div>
-  <router-view />
 </template>
 
 <style lang="scss">
@@ -15,16 +13,9 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#container__app {
+  margin: 10vh 10vw;
+  
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+import Card from 'primevue/card';
+
+import 'primevue/resources/themes/saga-blue/theme.css';       //theme
+import 'primevue/resources/primevue.min.css';                 //core css
+import 'primeicons/primeicons.css';                           //icons
+
+createApp(App)
+
+    .use(store)
+    .use(router)
+
+    .use(PrimeVue)
+
+    .component('Dialog', Dialog)
+    .component('Card', Card)
+
+.mount("#app");
